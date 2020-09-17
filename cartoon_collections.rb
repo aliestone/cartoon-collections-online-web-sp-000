95cf11b342d57a4fd4c2d03dee83a3b24e687b65
Alie Stone <helloaliestone@gmail.com>
2020-09-17 17:35:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,5 +7,5 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-  planeteer_calls.map! {|planeteer| planeteer.capitalize + "!"}
+  planeteer_calls.map {|planeteer| planeteer.capitalize + "!"}
 end
