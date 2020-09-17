fb14c680cd902263b18b61f2e6a4281c93fbdb84
Alie Stone <helloaliestone@gmail.com>
2020-09-17 17:36:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,6 @@
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |name, index|
-    print "#{index + 1} #{name}"
-  end
+  puts "#{index + 1} #{name}"
 end
 
 def summon_captain_planet(planeteer_calls)
